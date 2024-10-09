757eca3d1eb70fd6bcb0ace093176463fa2cf9e9
Flankiste <151396839+Flankiste@users.noreply.github.com>
2024-10-09 16:44:21 +0200

    code de tim

--- Exo7.c
+++ Exo7.c
@@ -7,6 +7,24 @@ int n;
 
 int main(){
 
-* le code de tim 
+    int n;
+    int Un = 1, Vn = 0;
+    int prev_U, prev_V;
+ 
+    printf("Entrez la valeur de n : ");
+    scanf("%d", &n);
+ 
+    for (int i = 1; i <= n; i++) {
+        prev_U = Un;
+        prev_V = Vn;
+ 
+        Vn = 2 * prev_U; // Vn = 2 * Un-1
+        Un = prev_V + 1; // Un = Vn-1 + 1
+    }
+ 
+    printf("Le terme U%d est : %d\n", n, Un);
+    printf("Le terme V%d est : %d\n", n, Vn);
+ 
+    return 0;
 
 }
